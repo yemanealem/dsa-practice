4ffd8091010f7319ed77fe8d992ad63210d14eb9
yemanealem <yemanea223@gmail.com>
2026-01-06 17:32:33 +0100

    single number problem using hashmap

--- array/SingleNumber.java
+++ array/SingleNumber.java
@@ -13,6 +13,24 @@ public class SingleNumber {
     public static void main(String[] args) {
         SingleNumber obj = new SingleNumber();
         int[] nums = {4, 1, 2, 1, 2};
-        System.out.println(obj.singleNumber(nums)); // Output: 4
+        System.out.println(obj.singleNumber(nums)); 
     }
 }
+
+/* 
+
+ Map<Integer, Integer> map = new HashMap<>();
+
+    for (int num : nums) {
+        map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+
+    for (int key : map.keySet()) {
+        if (map.get(key) == 1) {
+            return key;
+        }
+    }
+    return -1;
+
+
+*/
