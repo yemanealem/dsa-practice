ea3bb64a4d47f59948e5f283fff9bf38cd41f944
yemanealem <yemanea223@gmail.com>
2026-01-08 19:04:51 +0100

    reverse a string using recursive

--- string/ReverseString.java
+++ string/ReverseString.java
@@ -22,3 +22,31 @@ public class ReverseString {
         System.out.println("Reversed: " + new String(str));
     }
 }
+
+/*
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        reverseHelper(s, 0, s.length - 1);
+    }
+
+    private void reverseHelper(char[] s, int left, int right) {
+        if (left >= right) return; // Base case
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        reverseHelper(s, left + 1, right - 1); // Recur for inner substring
+    }
+
+    public static void main(String[] args) {
+        ReverseString rs = new ReverseString();
+        char[] str = {'h','e','l','l','o'};
+        rs.reverseString(str);
+        System.out.println(str); // olleh
+    }
+}
+
+
+
+
+*/
