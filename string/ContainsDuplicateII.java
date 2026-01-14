e756eae9c2f305a91a0c41bbe052884fdd9f90c1
yemanealem <yemanea223@gmail.com>
2026-01-14 20:06:43 +0100

    contains duplicate

---
+++ string/ContainsDuplicateII.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII {
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> lastSeen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer prevIndex = lastSeen.get(nums[i]);
+
+            if (prevIndex != null && i - prevIndex <= k) {
+                return true;
+            }
+
+            // Always update to latest index
+            lastSeen.put(nums[i], i);
+        }
+        return false;
+    }
+
+    // Main method to test the solution
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 1};
+        int k1 = 3;
+        System.out.println("Test 1: " + containsNearbyDuplicate(nums1, k1)); // true
+
+        int[] nums2 = {1, 0, 1, 1};
+        int k2 = 1;
+        System.out.println("Test 2: " + containsNearbyDuplicate(nums2, k2)); // true
+
+        int[] nums3 = {1, 2, 3, 1, 2, 3};
+        int k3 = 2;
+        System.out.println("Test 3: " + containsNearbyDuplicate(nums3, k3)); // false
+    }
+}
