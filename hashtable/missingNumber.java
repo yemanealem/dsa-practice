64290e62ea9c003cbff95ad20b93951d17e7caf7
yemanealem <yemanea223@gmail.com>
2026-01-16 18:25:34 +0100

    optimising missing number using xor

--- hashtable/missingNumber.java
+++ hashtable/missingNumber.java
@@ -1,20 +1,19 @@
-class Solution {
-    public int missingNumber(int[] nums) {
-       int n = nums.length + 1;
-
-        int[] hash = new int[n + 1];
-
-        // Store frequencies of elements
-        for (int i = 0; i < n - 1; i++) {
-            hash[nums[i]]++;
+public class MissingNumber {
+    public static int missingNumberXOR(int[] nums) {
+        int missing = nums.length; 
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i]; 
         }
+        return missing;
+    }
 
-        // Find the missing number
-        for (int i = 1; i <= n; i++) {
-            if (hash[i] == 0) {
-                return i;
-            }
-        }
-        return -1; 
+    public static void main(String[] args) {
+        int[] nums1 = {3, 0, 1};
+        int[] nums2 = {0, 1};
+        int[] nums3 = {9,6,4,2,3,5,7,0,1};
+
+        System.out.println("Missing number in nums1: " + missingNumberXOR(nums1)); 
+        System.out.println("Missing number in nums2: " + missingNumberXOR(nums2)); 
+        System.out.println("Missing number in nums3: " + missingNumberXOR(nums3)); 
     }
 }
