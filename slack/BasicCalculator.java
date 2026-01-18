cc8ca5300820041de08ba2cfcafc4e0d59bb2529
yemanealem <yemanea223@gmail.com>
2026-01-18 15:30:43 +0100

    wimporoved  basic calculator using slack

--- slack/BasicCalculator.java
+++ slack/BasicCalculator.java
@@ -1,53 +1,45 @@
 class Solution {
     public int calculate(String s) {
-        Deque<Integer> stack = new ArrayDeque<>();
-      
-        char operator = '+';
-      
+           if (s == null || s.isEmpty()) return 0;
+
+        int n = s.length();
+        int result = 0;       // Total result
+        int lastNumber = 0;   // Last number to handle * and /
         int currentNumber = 0;
-      
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-          
-            if (Character.isDigit(currentChar)) {
-                currentNumber = currentNumber * 10 + (currentChar - '0');
+        char operator = '+';  // Previous operator
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                currentNumber = currentNumber * 10 + (c - '0');
             }
-          
-            // Process the number when we encounter an operator or reach the end
-            // Skip spaces by checking if it's an operator or end of string
-            if (i == s.length() - 1 || 
-                currentChar == '+' || 
-                currentChar == '-' || 
-                currentChar == '*' || 
-                currentChar == '/') {
-              
-                // Apply the previous operator to the current number
-                if (operator == '+') {
-                    stack.push(currentNumber);
-                } else if (operator == '-') {
-                    stack.push(-currentNumber);
-                } else if (operator == '*') {
-                    // Pop the last number and multiply with current number
-                    stack.push(stack.pop() * currentNumber);
-                } else if (operator == '/') {
-                    // Pop the last number and divide by current number
-                    stack.push(stack.pop() / currentNumber);
+
+            // If it's an operator or last char, compute
+            if ((!Character.isDigit(c) && c != ' ') || i == n - 1) {
+                switch (operator) {
+                    case '+':
+                        result += lastNumber;   // add previous number
+                        lastNumber = currentNumber;
+                        break;
+                    case '-':
+                        result += lastNumber;
+                        lastNumber = -currentNumber;
+                        break;
+                    case '*':
+                        lastNumber *= currentNumber;
+                        break;
+                    case '/':
+                        lastNumber /= currentNumber;
+                        break;
                 }
-              
-                // Update operator for next iteration
-                operator = currentChar;
-              
-                // Reset current number for next parsing
+                operator = c;
                 currentNumber = 0;
             }
         }
-      
-        // Sum all numbers in the stack to get the final result
-        int result = 0;
-        while (!stack.isEmpty()) {
-            result += stack.pop();
-        }
-      
+
+        // Add the last number
+        result += lastNumber;
         return result;
     }
 }
