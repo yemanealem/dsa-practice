d6c93b2ca5543216261399466369948a81b9a258
yemanealem <yemanea223@gmail.com>
2026-01-20 17:30:13 +0100

    optimized crowler log folder

--- stack/MinOperations.java
+++ stack/MinOperations.java
@@ -1,26 +1,34 @@
 public class MinOperations {
-    
-}
-class Solution {
-    public int minOperations(String[] logs) {
-       String[] stack = new String[logs.length];
-    int top = -1; // empty stack
 
-    for (String log : logs) {
-        if (log.equals("../")) {
-            if (top >= 0) {
-                top--; // pop
+    // Method to calculate minimum operations to return to main folder
+    public int minOperations(String[] logs) {
+        // Manual stack using array
+        String[] stack = new String[logs.length]; // max possible depth
+        int top = -1; // empty stack
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (top >= 0) top--; // pop
+            } else if (log.equals("./")) {
+                // stay in current folder, do nothing
+            } else {
+                // push folder
+                stack[++top] = log;
             }
-        } else if (log.equals("./")) {
-            // stay in current folder, do nothing
-        } else {
-            // go into folder, push
-            top++;
-            stack[top] = log;
         }
+
+        // stack size = depth = steps to main folder
+        return top + 1;
     }
 
-    // number of folders deep = stack size
-    return top + 1; 
+    // Main method to test
+    public static void main(String[] args) {
+        MinOperations solution = new MinOperations();
+
+        // Test example
+        String[] logs = {"d1/", "d2/", "../", "d21/", "./"};
+        int result = solution.minOperations(logs);
+
+        System.out.println("Minimum operations to return to main folder: " + result);
     }
 }
