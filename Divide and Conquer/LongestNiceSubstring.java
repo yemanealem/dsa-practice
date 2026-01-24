8b1e0116c1d02b89a02b7930aba5babd60bdff95
yemanealem <yemanea223@gmail.com>
2026-01-24 17:01:51 +0100

    refactored lingest nice substring

--- Divide and Conquer/LongestNiceSubstring.java
+++ Divide and Conquer/LongestNiceSubstring.java
@@ -3,32 +3,38 @@ public class LongestNiceSubstring {
     public static void main(String[] args) {
         LongestNiceSubstring solver = new LongestNiceSubstring();
         String s = "YazaAay";
-        String result = solver.longestNiceSubstring(s);
-        System.out.println("Longest Nice Substring: " + result);
+        System.out.println("Longest Nice Substring: " + solver.longestNiceSubstring(s));
     }
 
     public String longestNiceSubstring(String s) {
-        if (s.length() < 2) return ""; // Base case: string too short to be nice
+        int[] res = helper(s, 0, s.length());
+        return s.substring(res[0], res[0] + res[1]);
+    }
 
-        // Build a set of characters in the current string
-        java.util.Set<Character> set = new java.util.HashSet<>();
-        for (char c : s.toCharArray()) {
-            set.add(c);
+    // Returns {startIndex, length} of longest nice substring in s[left..right-1]
+    private int[] helper(String s, int left, int right) {
+        if (right - left < 2) return new int[]{left, 0}; // too short to be nice
+
+        // Count presence of each character in this substring
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
+        for (int i = left; i < right; i++) {
+            char c = s.charAt(i);
+            if (Character.isLowerCase(c)) lower[c - 'a'] = true;
+            else upper[c - 'A'] = true;
         }
 
-        // Check each character: if it breaks the nice property, split
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = left; i < right; i++) {
             char c = s.charAt(i);
-            if (set.contains(Character.toLowerCase(c)) && set.contains(Character.toUpperCase(c))) {
-                continue; // OK
+            if (Character.isLowerCase(c) && !upper[c - 'a'] || Character.isUpperCase(c) && !lower[c - 'A']) {
+                // split around this character
+                int[] leftRes = helper(s, left, i);
+                int[] rightRes = helper(s, i + 1, right);
+                return leftRes[1] >= rightRes[1] ? leftRes : rightRes;
             }
-            // Split at this character and solve recursively
-            String left = longestNiceSubstring(s.substring(0, i));
-            String right = longestNiceSubstring(s.substring(i + 1));
-            return left.length() >= right.length() ? left : right;
         }
 
         // All characters are nice
-        return s;
+        return new int[]{left, right - left};
     }
 }
