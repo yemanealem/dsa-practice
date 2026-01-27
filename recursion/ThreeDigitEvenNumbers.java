f1bbc1a08c3c2bd601235f4dba275b11de64ad18
yemanealem <yemanea223@gmail.com>
2026-01-27 23:53:43 +0100

    refactored three digit even number

--- recursion/ThreeDigitEvenNumbers.java
+++ recursion/ThreeDigitEvenNumbers.java
@@ -2,30 +2,34 @@ import java.util.*;
 
 public class ThreeDigitEvenNumbers {
 
+    /**
+     * Find all unique 3-digit even numbers that can be formed from the given digits.
+     */
     public int[] findEvenNumbers(int[] digits) {
         int[] cnt = new int[10];
 
-        // Count available digits
+        // Count how many times each digit is available
         for (int x : digits) {
             cnt[x]++;
         }
 
         List<Integer> ans = new ArrayList<>();
 
-        // Iterate through all 3-digit even numbers
-        for (int x = 100; x < 1000; x += 2) {
+        // Iterate through all 3-digit even numbers (100 to 998)
+        for (int x = 100; x < 1000; x += 2) {  // only even numbers
             int[] cnt1 = new int[10];
             int temp = x;
 
-            // Count digits of current number
+            // Count digits of the current number
             while (temp > 0) {
                 cnt1[temp % 10]++;
                 temp /= 10;
             }
 
+            // Check if we have enough digits in the input to form this number
             boolean ok = true;
             for (int i = 0; i < 10; i++) {
-                if (cnt1[i] > cnt[i]) {
+                if (cnt1[i] > cnt[i]) {  // input doesn't have enough of digit i
                     ok = false;
                     break;
                 }
@@ -36,10 +40,13 @@ public class ThreeDigitEvenNumbers {
             }
         }
 
+        // Convert List<Integer> to int[]
         return ans.stream().mapToInt(i -> i).toArray();
     }
 
-    // Main method for testing
+    /**
+     * Main method for testing the solution
+     */
     public static void main(String[] args) {
         ThreeDigitEvenNumbers solution = new ThreeDigitEvenNumbers();
 
