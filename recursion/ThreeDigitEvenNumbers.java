a236c6a27ebb8bd3ec25ad2448b710ae211f9b8e
yemanealem <yemanea223@gmail.com>
2026-01-27 23:52:46 +0100

    refactored three digit even number

--- recursion/ThreeDigitEvenNumbers.java
+++ recursion/ThreeDigitEvenNumbers.java
@@ -1,21 +1,51 @@
+import java.util.*;
+
 public class ThreeDigitEvenNumbers {
 
-    public static void findEven(int n) {
-        // Base case: stop when number exceeds 999
-        if (n > 999) {
-            return;
+    public int[] findEvenNumbers(int[] digits) {
+        int[] cnt = new int[10];
+
+        // Count available digits
+        for (int x : digits) {
+            cnt[x]++;
         }
 
-        // Check if number is even
-        if (n % 2 == 0) {
-            System.out.print(n + " ");
+        List<Integer> ans = new ArrayList<>();
+
+        // Iterate through all 3-digit even numbers
+        for (int x = 100; x < 1000; x += 2) {
+            int[] cnt1 = new int[10];
+            int temp = x;
+
+            // Count digits of current number
+            while (temp > 0) {
+                cnt1[temp % 10]++;
+                temp /= 10;
+            }
+
+            boolean ok = true;
+            for (int i = 0; i < 10; i++) {
+                if (cnt1[i] > cnt[i]) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok) {
+                ans.add(x);
+            }
         }
 
-        // Recursive call
-        findEven(n + 1);
+        return ans.stream().mapToInt(i -> i).toArray();
     }
 
+    // Main method for testing
     public static void main(String[] args) {
-        findEven(100);
+        ThreeDigitEvenNumbers solution = new ThreeDigitEvenNumbers();
+
+        int[] digits = {2, 1, 3, 0};
+        int[] result = solution.findEvenNumbers(digits);
+
+        System.out.println(Arrays.toString(result));
     }
 }
